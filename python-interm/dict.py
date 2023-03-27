c289140ee939501305c90c2ed7b620f4a56262eb
mathmer <123316938+mathmer@users.noreply.github.com>
2023-03-27 11:07:54 +0600

    Update

---
+++ python-interm/dict.py
@@ -0,0 +1,58 @@
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = dict(zip(countries, capitals))
+
+# Print europe
+print(europe)
+
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+
+europe['italy'] = 'rome'
+# Print out italy in europe
+
+print('italy' in europe)
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
+
+
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+
+europe['germany'] = 'berlin'
+# Remove australia
+
+del(europe['australia'])
+# Print europe
+print(europe)
+
+
+
+
---
+++ python-interm/sizes.py
@@ -0,0 +1,36 @@
+# Import numpy as np
+
+import numpy as np
+# Store pop as a numpy array: np_pop
+np_pop = np.array()
+
+# Double np_pop
+np_pop *= 2
+
+# Update: set s argument to np_pop
+plt.scatter(gdp_cap, life_exp, s = pop)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
+
+# Display the plot
+plt.show()
+
+
+
+# Specify c and alpha inside plt.scatter()
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col, alpha = 0.8)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Show the plot
+plt.show()
